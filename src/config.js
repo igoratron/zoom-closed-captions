6165201c275bef9abe8135fcbc3714560d5568fc
Igor Czerwinski <657586+igoratron@users.noreply.github.com>
2020-05-08 15:12:25 +0100

    Attempt to use a custom TURN server

--- src/config.js
+++ src/config.js
@@ -1,15 +1,16 @@
+const ec2 = {
+  urls: "turn:63.33.211.117:34534",
+  credential: "test",
+  username: "test"
+};
+
 module.exports = {
   CONNECTION_STATE: {
     CONNECTING: Symbol("connecting"),
     CONNECTED: Symbol("connected")
   },
   SOCKET_URL: "https://rtcmulticonnection.herokuapp.com:443/",
-  ICE_SERVERS: []
+  ICE_SERVERS: [ec2]
 };
 //
 // connection.socketURL = 'http://3.10.215.245:9000/';
-// connection.iceServers = [{
-//   urls: 'turn:3.10.215.245:34534',
-//   credential: 'test',
-//   username: 'test',
-// }];
